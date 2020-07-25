7b31b1597c0b13894f7a9d00a323c8c5493e6452
Bhakti Al Akbar <balapa95@gmail.com>
2020-07-25 18:32:10 +0700

    update homepage design, convert old homepage as coming soon page

--- src/gatsby-theme-blog/components/post.js
+++ src/gatsby-theme-blog/components/post.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
-import Layout from "./layout"
+import Layout from "../../components/layout/layout"
 import SEO from "../../components/seo"
 import PostFooter from "./post-footer"
 
@@ -22,9 +22,9 @@ const Post = ({
         frontmatter={frontmatter}
         isBlogPost
       />
-    <div className="post-header mb-16">
-      <h1 className="text-4xl font-bold mb-3">{post.title}</h1>
-      <p className="text-gray-600 -mt-2">{post.date}</p>
+    <div className="mb-16 post-header">
+      <h1 className="mb-3 text-4xl font-bold">{post.title}</h1>
+      <p className="-mt-2 text-gray-600">{post.date}</p>
     </div>
 
 		<article className="article space-y-8">
